a62fbdc4fe0313ce7128c0b68d6aebd7bebd7ab9
R, Madanika K <Madanika_K_R@Dell.com>
2022-01-08 21:44:16 +0530

    Add project files.

---
+++ ConsoleSample project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleSample_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets learn c#!");
+            Console.ReadKey();
+        }
+    }
+}
